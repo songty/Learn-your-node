53a425b02d0208a6044bc5d5dcf1f542c8da23c0
Tian Song <tian.y.song@gmail.com>
2014-06-16 20:33:17 -0700

    passed the uppdercase challenge

--- Learyounode/http-uppercase.js
+++ Learyounode/http-uppercase.js
@@ -2,8 +2,14 @@ var http = require('http');
 var fs = require('fs');
 
 var server = http.createServer(function(req, res) {
-	console.log(req);
-	console.log(res);
+	if (req.method != 'POST') 
+		return res.end('send me a POSY\n');
+	// console.log(req.method);
+	req.on('data', function(data) {
+		var things = (data.toString());
+		var result = things.toUpperCase();
+		res.write(result);
+	});
 });
 
-server.listen(process.argv[2]);
+server.listen(Number(process.argv[2]));
