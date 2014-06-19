3ffda374cbb569312025f7f491f91a371516a167
Tian Song <tian.y.song@gmail.com>
2014-06-18 21:14:52 -0700

    PASSED all challenges.

--- Learyounode/http-json-api-server.js
+++ Learyounode/http-json-api-server.js
@@ -7,9 +7,23 @@ var server = http.createServer(function(req, res) {
 	console.log(blah);
 	console.log(blah.query);
 	console.log(blah.path);
-	server.listen(blah.query, function() {
-		console.log('he');
-	});
+	var date = new Date(blah.query.iso);
+
+	// console.log(Date.getTime(blah.query.iso));
+	if (req.method === 'GET' && blah.pathname === '/api/parsetime' && blah.query.iso) {
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify({
+			'hour': date.getHours(),
+			'minute': date.getMinutes(),
+			'second': date.getSeconds()
+		}));
+
+	}
+	if (req.method === 'GET' && blah.pathname === '/api/unixtime') {
+			res.end(JSON.stringify({
+			'unixtime': Date.parse(date.toISOString())
+		}));
+	}
 	// src.pipe(res);
 });
 
